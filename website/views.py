ea38674dd71d345a600261e579691c6006048cc7
mich360 <65708357+mich360@users.noreply.github.com>
2023-08-22 10:47:42 +0900

    Update views.py

--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ class IndexView(TemplateView):
     
     def get_context_data(self):
         ctxt = super().get_context_data()
-        ctxt["username"] = "太郎"
+        ctxt["username"] = "ヌートバー"
         return ctxt
     
 
@@ -16,12 +16,12 @@ class AboutView(TemplateView):
     def get_context_data(self):
         ctxt = super().get_context_data()
         ctxt["skills"] = [
-            "Python",
-            "C++",
-            "Javascript",
-            "Rust",
-            "Ruby",
-            "PHP"
+            "2012年にプロ入りし、2013年には最優秀新人賞を受賞",
+            "2014年には、パシフィック・リーグで最多勝、最優秀防御率、最多奪三振、最多完投を達成し、日本シリーズでもMVPを獲得",
+            "2016年には、リーグ戦で22本の本塁打を放ち、MVPを受賞",
+            "2017年には、自身初の二刀流で、打者として22本の本塁打を打ち、投手としても10勝を挙げました",
+            "",
+            ""
         ]
         ctxt["num_services"] = 1234567
         return ctxt
